ee1d9e024c51b16fa68c9c77e986c9b2469f46ec
Devin <devin@handspiker-wade.com>
2019-03-11 21:04:35 -0300

    Move sqlite functions to separate file

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const PlugAPI = require('plugapi');
-const sqlite3 = require('sqlite3');
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 const logger = new (require("jethro"))();
 
-require('dotenv').config()
-
-const DB_VERSION = 2;
-
+require('dotenv').config();
 const ROOM = typeof argv.r === 'string' ? argv.r : process.env.PLUGDJ_ROOM;
 const LOGGER_DEFAULT_SOURCE = 'StatsBot';
 
@@ -47,53 +43,10 @@ const bot = new PlugAPI(botParams);
 bot.setLogger(logger);
 bot.deleteCommands = false;
 
+const dataHandle = require('./sqlite')(bot, logger);
 
-logger.info(LOGGER_DEFAULT_SOURCE,'Creating or verifying database.');
-const db = new sqlite3.Database('./stats.sqlite');
-
-db.serialize();
-db.exec('PRAGMA foreign_keys = FALSE')
-    .exec('PRAGMA optimize')
-    .exec('PRAGMA auto_vacuum = FULL');
-
-db.get('PRAGMA user_version;', (err, row) => {
-    if (err) throw err;
-    let statement;
-
-    switch (row.user_version * 1) {
-        case 0:
-            // This case may run on databases that existed for DB versioning. Cannot assume the tables don't exist.
-            db.exec('CREATE TABLE IF NOT EXISTS `dj` ( `id` INTEGER NOT NULL, `username` TEXT, PRIMARY KEY(`id`) ) WITHOUT ROWID')
-                .exec('CREATE TABLE IF NOT EXISTS "play" ( `song_id` INTEGER NOT NULL, `room_slug` INTEGER NOT NULL, `dj_id` INTEGER NOT NULL, `unixdate` INTEGER NOT NULL, `woots` INTEGER NOT NULL DEFAULT 0, `grabs` INTEGER NOT NULL DEFAULT 0, `mehs` INTEGER NOT NULL DEFAULT 0, `skipped` INTEGER NOT NULL DEFAULT 0, `listeners` INTEGER NOT NULL DEFAULT 0 )')
-                .exec('CREATE TABLE IF NOT EXISTS `room` ( `slug` TEXT NOT NULL UNIQUE, `title` TEXT )')
-                .exec('CREATE TABLE IF NOT EXISTS "song" ( `title` TEXT, `cid` TEXT, `author` TEXT, `id` INTEGER NOT NULL, PRIMARY KEY(`id`) ) WITHOUT ROWID');
-        case 1:
-            db.exec('CREATE TABLE song_copied AS SELECT * FROM song WHERE 0 = 0;')
-                .exec('DROP TABLE song;')
-                .exec('CREATE TABLE "song" ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `title` TEXT, `cid` TEXT, `author` TEXT )')
-                .exec('INSERT into song (author, title, cid)  select max(author), max(title), cid FROM song_copied GROUP BY cid');
-
-            statement = db.prepare('UPDATE play SET song_id = ? WHERE room_slug = ? AND unixdate = ?');
-            db.each('SELECT room_slug, unixdate, song_id FROM play;', (err, playRow) => {
-                if (err) throw err;
-
-                db.get('SELECT song.id FROM song JOIN song_copied ON song.cid = song_copied.cid WHERE song_copied.id = ?', [playRow.song_id], (err, songRow) => {
-                    statement.run([songRow.id, playRow.room_slug, playRow.unixdate]);
-                });
-            }, () => {
-                db.exec('DROP TABLE song_copied');
-            });
-
-
-        default: 
-            break;
-    }
-
-    db.run('PRAGMA user_version = ' + DB_VERSION);
-
-    logger.info(LOGGER_DEFAULT_SOURCE, `Attempting to connect to "${ROOM}"`);
-    bot.connect(ROOM);
-});
+logger.info(LOGGER_DEFAULT_SOURCE, `Attempting to connect to "${ROOM}"`);
+bot.connect(ROOM);
 
 // Sleep mode detection
 let lastHeartbeat = Date.now();
@@ -109,56 +62,8 @@ setInterval(() => {
 function cleanup () {
     logger.info(LOGGER_DEFAULT_SOURCE, 'Performing cleanup');
     bot.close(false); logger.debug(LOGGER_DEFAULT_SOURCE, 'Closing PlugDJ connection');
-    db.close(); logger.debug(LOGGER_DEFAULT_SOURCE, 'Closing SQLite database');
-
+    dataHandle.cleanup();
     fs.writeFileSync('config.json', JSON.stringify(config));
-}
-
-function newDj(db, dj) {
-    logger.debug(LOGGER_DEFAULT_SOURCE, 'Attempting to update dj ' + dj.id);
-    db.run('UPDATE dj SET username = ? WHERE id = ?', dj.username, dj.id, function (err) { // Must be long form function to use `this.changes`
-        if (err) throw err;
-
-        if (this.changes == 0) {
-            logger.debug(LOGGER_DEFAULT_SOURCE, 'Attempting to insert dj ' + dj.id);
-            db.run('INSERT INTO dj (username, id) VALUES (?, ?)', dj.username, dj.id, (err) => {
-                if (err) logger.error(LOGGER_DEFAULT_SOURCE, err);
-            });
-        }
-    });
-}
-
-function newSong(db, media) {
-    db.get('SELECT id FROM song WHERE cid = ?', media.cid, (err, row) => {
-        if (err) throw err;
-
-        if (!row) {
-            logger.debug(LOGGER_DEFAULT_SOURCE, 'Attempting to insert song ' + media.id);
-            db.run('INSERT INTO song (cid, author, title) VALUES (?,?,?)', media.cid, media.author, media.title);
-        } else {
-            logger.debug(LOGGER_DEFAULT_SOURCE, 'Attempting to update song ');
-            db.run('UPDATE song  SET cid = ?, author = ?, title = ? WHERE cid = ?', media.cid, media.author, media.title, media.cid);
-        }
-    });
-}
-
-function insertPlay(db, room, media, score, user) {
-    if (media && 'cid' in media) {
-        logger.debug(LOGGER_DEFAULT_SOURCE, 'Attempting to insert new play for song ' + media.cid);
-        if (user) {
-            db.get('SELECT id FROM song WHERE cid = ?', [media.cid], (err, row) => {
-                if (err || !row) throw 'Song ID mismatch';
-
-                db.run('INSERT INTO play (song_id, room_slug, unixdate, dj_id, woots, grabs, mehs, skipped, listeners) VALUES (?, ?,?,?,?,?,?,?,?)', 
-                    [row.id, room, Math.floor(Date.now() / 1000), user.id, score.positive, score.grabs, score.negative, score.skipped ? 1 : 0, score.listeners]
-                );
-            });
-
-            return true;
-        }
-    }
-
-    return false;
 }
 
 const shutdown = () => {
@@ -189,8 +94,8 @@ const reconnect = () => {
             bot.connect(ROOM); 
 
             if (bot.getDJ() && bot.getMedia()) {
-                newDj(db, bot.getDJ());
-                newSong(db, bot.getMedia());
+                dataHandle.newDj(bot.getDJ());
+                dataHandle.newSong(bot.getMedia());
             }
             _isReconnecting = false;
         }, 1000);
@@ -244,12 +149,12 @@ bot.on(PlugAPI.events.ADVANCE, (data) => {
         }
 
         if (data.currentDJ) {
-            newDj(db, data.currentDJ);
-            newSong(db, data.media);
+            dataHandle.newDj(data.currentDJ);
+            dataHandle.newSong(data.media);
         }
 
         if (data.lastPlay) {
-            insertPlay(db, ROOM, data.lastPlay.media, data.lastPlay.score, data.lastPlay.dj);
+            dataHandle.insertPlay(ROOM, data.lastPlay.media, data.lastPlay.score, data.lastPlay.dj);
         }
     }
 });
